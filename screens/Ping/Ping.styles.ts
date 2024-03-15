6ea588e0b57d9fb41c402be9be41393d9f83fde8
Kirk Santos <kirkalyn13@gmail.com>
2024-03-15 18:28:49 +0800

    fix: ping screen height

--- screens/Ping/Ping.styles.ts
+++ screens/Ping/Ping.styles.ts
@@ -3,8 +3,9 @@ import { COLOR_LIGHT_PRIMARY } from '../../styles/Colors.styles';
 
 const styles = StyleSheet.create({
     container: {
-    paddingHorizontal: 8,
-    backgroundColor: COLOR_LIGHT_PRIMARY,
+        height: "100%",
+        paddingHorizontal: 8,
+        backgroundColor: COLOR_LIGHT_PRIMARY,
     },
   });
 
